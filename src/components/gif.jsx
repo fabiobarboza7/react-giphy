6e42d979fbb6dd3f58d334a1862526e45b7aca49
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-05-17 17:14:57 -0300

    fixing react class to function

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Gif extends React.PureComponent {
-    render () {
-        const { id } = this.props;
-        const src = `https://media1.giphy.com/media/${id}/200.gif`;
-        return (
-            <img src={src} alt="gif" className="gif" />
-        );
-    }
-}
+const Gif = (props) => {
+    const { id } = props;
+    const src = `https://media1.giphy.com/media/${id}/200.gif`;
+    return (
+        <img src={src} alt="gif" className="gif" />
+    );
+};
 
 export default Gif;
